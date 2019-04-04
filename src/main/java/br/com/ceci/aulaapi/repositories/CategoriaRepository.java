35d9e28cdaa435bf3d1c7b3061302b9e14ebd5a8
Aluno <aluno@eteclencois.com.br>
2019-04-04 11:07:43 -0300

    commit inicial projeto aulaapi

--- src/main/java/br/com/ceci/aulaapi/repositories/CategoriaRepository.java
+++ src/main/java/br/com/ceci/aulaapi/repositories/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package br.com.ceci.aulaapi.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import br.com.ceci.aulaapi.domain.Categoria;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
 
-	
+public  Page<Categoria> findByNomeContaining(String nome, Pageable pageable);
 	
 }
