2a7dbe5ad825af390331a783c491ae9a40d0925e
Aluno <aluno@eteclencois.com.br>
2019-04-18 12:24:06 -0300

    commit inicial projeto aulaapi service

--- src/main/java/br/com/ceci/aulaapi/config/SecurityConfig.java
+++ src/main/java/br/com/ceci/aulaapi/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package br.com.ceci.aulaapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +16,12 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 	 @Override
 	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 	    	auth.inMemoryAuthentication()
-	    	.withUser("admin").password("admin").roles("ROLE");
+	    	.withUser("admin").password(passwordEnconder().encode("admin")).roles("ADMIN");
 	    }
+	 @Bean
+	 public PasswordEncoder passwordEnconder() {
+		 return new BCryptPasswordEncoder();
+	 }
 	    @Override
 	    protected void configure(HttpSecurity http) throws Exception {
 	    	http.authorizeRequests()
